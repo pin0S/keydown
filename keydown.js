017941d7dada17ec2a69c0e67499ec51d60d1c13
pin0S <pete.g.lynch@gmail.com>
2020-12-19 17:44:33 +1100

    other key events now showing in document

--- keydown.js
+++ keydown.js
@@ -3,8 +3,15 @@
 // keydown event
 document.addEventListener('keydown', (e) => {
     
+    console.log(e)
+
+    //info container
+    const info = document.querySelector('.info-container')
     //show big key
     document.querySelector('h2').textContent = e.key
+    info.firstElementChild.lastElementChild.textContent = e.key
+    info.children[1].lastElementChild.textContent = e.which
+    info.lastElementChild.lastElementChild.textContent = e.code 
 
 
 });
